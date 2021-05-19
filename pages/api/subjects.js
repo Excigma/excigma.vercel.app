f18d696b4c1ea0e43501a3570f028bf1a16bf527
Excigma <30280397+Excigma@users.noreply.github.com>
2021-05-19 13:05:54 +1200

    fix: Fix api/ proxy

--- pages/api/subjects.js
+++ pages/api/subjects.js
@@ -2,7 +2,7 @@
 const fetch = require("petitio")
 
 export default async (req, res) => {
-  const res = await fetch("https://pastpaperarmyknife.up.railway.app/api/subjects").send().json()
+  const response = await fetch("https://pastpaperarmyknife.up.railway.app/api/subjects").send().json()
   res.statusCode = 200
-  res.json(res)
+  res.json(response)
 }
