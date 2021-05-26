b57dc5645cd8efe016d6035e97e0731fdb5f27f4
Excigma <30280397+Excigma@users.noreply.github.com>
2021-05-27 10:42:27 +1200

    fix: change subheading to p

--- src/components/Subheading.js
+++ src/components/Subheading.js
@@ -3,6 +3,6 @@ import * as React from 'react';
 
 export default function Subheading(props) {
     return (
-        <Typography variant="h6" component="h4" {...props} />
+        <Typography variant="h6" component="p" {...props} />
     );
 }
