97e39b4d77de025501291d41499b5626b09a92e2
Excigma <30280397+Excigma@users.noreply.github.com>
2021-05-26 13:01:31 +1200

    add: placeholder files to make dir not empty

---
+++ src/pages/notes/2021/9609_-_Business_Studies/Both_Papers/Placeholder.js
@@ -0,0 +1,17 @@
+import Heading from 'components/Heading';
+import Subheading from 'components/Subheading';
+import * as React from 'react';
+
+export default function Page() {
+    return (
+        <>
+            <Heading>
+                Empty page used as placeholder
+            </Heading>
+
+            <Subheading>
+                Todo :)
+            </Subheading>
+        </>
+    );
+}
