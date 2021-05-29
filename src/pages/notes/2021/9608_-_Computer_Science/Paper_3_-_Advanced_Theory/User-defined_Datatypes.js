fc6a6a4d86b8950cc855357c5bb0782e042bb585
Excigma <30280397+Excigma@users.noreply.github.com>
2021-05-29 23:04:50 +1200

    new: update userdef'd datatypes

--- src/pages/notes/2021/9608_-_Computer_Science/Paper_3_-_Advanced_Theory/User-defined_Datatypes.js
+++ src/pages/notes/2021/9608_-_Computer_Science/Paper_3_-_Advanced_Theory/User-defined_Datatypes.js
@@ -2,48 +2,120 @@ import { Typography } from '@material-ui/core';
 import Codeblock from 'components/Codeblock';
 import Heading from 'components/Heading';
 import Subheading from 'components/Subheading';
+import UnorderedList from 'components/UnorderedList';
+import UnorderedListItem from 'components/UnorderedListItem';
 import * as React from 'react';
+
 
 export default function Page() {
     return (
         <>
             <Heading>
-                User-defined data types
+                Syllabus:
             </Heading>
 
+            <UnorderedList>
+                <UnorderedListItem>
+                    Show understanding of why user-defined types are necessary
+                </UnorderedListItem>
+                <UnorderedListItem>
+                    Define and use non-composite types: enumerated, pointer
+                </UnorderedListItem>
+                <UnorderedListItem>
+                    Define and use composite data types: set, record and class/object
+                </UnorderedListItem>
+                <UnorderedListItem>
+                    Choose and design an appropriate user-defined data type for a given problem
+                </UnorderedListItem>
+            </UnorderedList>
+
             <Subheading>
-                3.1.1 User-defined data types
+                Show understanding of why user-defined types are necessary
             </Subheading>
 
             <Typography>
-                You can able to define your own datatypes inside VB and Pseudocode
+                (This needs confirming)
+                Programmers can choose to use an integer datatype for example, which will be the same for ever program, however user-defined data types are needed when the programmer uses a record, as they are not the same for all programs.
             </Typography>
+
+            <Subheading>
+                Define and use <b>non-composite</b> types: Enumerated, Pointer
+            </Subheading>
+
+            <Subheading>
+                Enums, aka Enumerated
+            </Subheading>
 
             <Typography>
-                In VB, they are called <code>Structure</code>s
+                An <b>Enumerated</b> data type represents a list of possible values. Even though the values inside the enum may look like a string, they are not and <b>should not</b> be enclosed in quote marks. The order of values inside an enum is important and can be used in programs compare values.
             </Typography>
-            <Codeblock language="visual-basic">
+
+            <Typography>An example would be an enum for days of the week and to compare the day of the week to see if it is greater than the day before.</Typography>
+
+            <Codeblock language="psuedocode">
                 {`
-' Defining your own datatype
-Structure Cat
-    Dim Name As String
-	Dim Birthdate As Date
-	Dim Breed As String
-End Structure
+TYPE
+	<CustomTypeName> = (<Value1>, <Value2>, <Value3>, <etc ...>)
+	// <etc>...
+ENDTYPE
 
-' You can use the student structure here
-' You the syntax for declaration is just like any other datatype
-Dim MyCat As Cat
-MyCat.Name = "Lorem Ipsum"
+// Example
 
-' You can additionally also have an array of a structure type
-Dim Cats(99) As Cat
-Cats(1).Name = "abcd"
-                `}
+TYPE
+	TDaysOfWeek = (Monday, Tuesday, Wednesday, Thursday, Friday)
+	TCompassPoints = (North, East, South, West)
+ENDTYPE
+
+DECLARE Direction : TCompassPoints
+DECLARE DayOfWeek : TDaysOfWeek
+
+Direction <- East
+
+IF Day > Friday THEN
+    // It is the weekend.
+ENDIF
+
+            `}
             </Codeblock>
 
+            <Subheading>
+                Pointer
+            </Subheading>
+
             <Typography>
-                In Psuedocode, they are called <code>Record</code>s
+                The pointer datatype is used to reference a location in memory. It can be used to create data structures that are dynamic (can change size)
+            </Typography>
+
+            <Codeblock language="psuedocode">
+                {`
+TYPE <PointerTypeName> = ^<TypeName>
+
+// Example
+
+TYPE TDayOfTheWeek = ^TDaysOfWeek
+DECLARE DayPointer : TDayOfTheWeek
+
+// DayPointer will now store the position of DayOfWeek's memory location
+DayPointer <- ^DayOfWeek
+
+// If you need to get the contents from the memory location, you will need to "dereference" it.
+DECLARE MyDay : TDayOfTheWeek
+MyDay <- DayPointer^
+// TODO: Check with teacher. The OneNote may be wrong
+// MyDay now contains the value from the value stored at DayPointer
+            `}
+            </Codeblock>
+
+            <Subheading>
+                Define and use <b>composite</b> data types: Set, Record and Class/Object
+            </Subheading>
+
+            <Typography>
+                You can able to define your own datatypes in Pseudocode (and VB - However this is not needed.)
+            </Typography>
+
+            <Typography>
+                One of the most useful and used user-defined datatype. It allows the programmer to group together a fixed number of values, of which can be of different datatypes.
             </Typography>
 
             <Codeblock language="psuedocode">
@@ -74,6 +146,10 @@ Cats[1].Name <- "abcd"
                 Sets
             </Subheading>
 
+            <Typography>
+                Sets allow you to store unique values, and allow you to run mathematical operations from set theory, including checking if an element is in the set, adding the element to the set, removing the element from the set, union, difference and intersection.
+            </Typography>
+
             <Codeblock language="psuedocode">
                 {`
 TYPE <CustomTypeName> = SET OF <Basetype>
@@ -86,31 +162,14 @@ DEFINE Vowels("a", "e", "i", "o", "u") : <CustomTypeName>
                 `}
             </Codeblock>
 
-
             <Subheading>
-                Enums
+                Class/Object
             </Subheading>
 
-            <Codeblock language="psuedocode">
-                {`
-TYPE
-	<CustomTypeName> = (<Value1>, <Value2>, <Value3>, <etc ...>)
-	// <etc>...
-ENDTYPE
+            <Typography>
+                TODO. Maybe check it out in paper 4 i think later im not even done yet AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            </Typography>
 
-// Example
-
-TYPE
-	TDaysOfWeek = (Monday, Tuesday, Wednesday, Thursday, Friday)
-	TCompassPoints = (North, East, South, West)
-ENDTYPE
-
-DECLARE Direction : TCompassPoints
-DECLARE DayOfWeek : TDaysOfWeek
-
-Direction <- East
-            `}
-            </Codeblock>
         </>
     );
 }
