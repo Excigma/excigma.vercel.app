c8a606fba5d955407b55abab3a0883c498afb331
Excigma <30280397+Excigma@users.noreply.github.com>
2021-05-30 22:55:53 +1200

    fix: wsdfdkl idk fix some files formatting, nothing much

--- src/pages/notes/2022/Nothing_yet.js
+++ src/pages/notes/2022/Nothing_yet.js
@@ -1,13 +1,12 @@
-import { Stack } from '@material-ui/core';
 import Subheading from 'components/Subheading';
 import * as React from 'react';
 
 export default function Page() {
     return (
-        <Stack spacing={3}>
-            <Subheading variant="h2" fontWeight="fontWeightMedium">
+        <>
+            <Subheading>
                 There's nothing here yet :)
             </Subheading>
-        </Stack>
+        </>
     );
 }
