c008b27075cce4f2a0cef54b22f6fe9d629b2836
Excigma <30280397+Excigma@users.noreply.github.com>
2021-05-31 09:26:33 +1200

    update: notes

--- src/pages/notes/2021/9708_-_Economics/Macroeconomy/Aggregate_Demand_and_Supply.js
+++ src/pages/notes/2021/9708_-_Economics/Macroeconomy/Aggregate_Demand_and_Supply.js
@@ -31,6 +31,19 @@ export default function Page() {
             <Typography>
                 The largest component of aggregate demand is the spending of money on goods and services, also known as consumption. The most influential factor that may cause a change in consumer expenditure is probably a change in income.
             </Typography>
+
+            <Typography>
+                Investment is the spending of money by firms to buy capital goods. This is also an important factor as not only does this increase the AD in the short run, it can help increase production of goods in the long run - if say new machinery is brought. Firms are influenced by their projection of future demand, an forecast increase in demand will cause them to buy new machinery to meet it.
+            </Typography>
+
+            <Typography>
+                Government expenditure or government spending is the money the government spends on goods and services to better the country by trying to provide improved infrastructure to promote growth and also by helping those in need.
+            </Typography>
+
+            <Typography>
+                Imports and exports are the goods and services we provide or purchase from other countries. Firms may buy goods and services from outside the country, leading to money leaving the country. On the contrary, goods can be exported internationally, causing firms to receive a profit from international firms or consumers.
+            </Typography>
+
             <Ul>
                 <Li>C + I = Private sector</Li>
                 <Li>G = Public sector</Li>
@@ -59,7 +72,9 @@ export default function Page() {
                 Aggregate Supply analysis
             </Subheading>
 
-
+            <Typography>
+                TODO: Add details
+            </Typography>
         </>
     );
 }
