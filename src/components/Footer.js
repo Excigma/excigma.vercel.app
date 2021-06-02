5dd08e651d52ca2a32b198f83a063314404e70f5
Excigma <30280397+Excigma@users.noreply.github.com>
2021-06-02 12:16:16 +1200

    fix: mildly triggered

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
     return (
         <footer>
             <Box my={5}>
-                <Stack alignItems="center" spacing={2}>
+                <Stack alignItems="center">
                     <Typography variant="h6" component="p" align="center">
                         Excigma's Webpage
                     </Typography>
