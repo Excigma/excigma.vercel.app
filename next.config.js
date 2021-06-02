f4ebb3206cbebfb217783968e0715d8fe36acfb8
Excigma <30280397+Excigma@users.noreply.github.com>
2021-06-03 11:55:46 +1200

    new: try using esbuild instead

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+const { ProvidePlugin } = require('webpack');
+
+function useEsbuildMinify(config, options) {
+    const terserIndex = config.optimization.minimizer.findIndex(minimizer => (minimizer.constructor.name === 'TerserPlugin'));
+    if (terserIndex > -1) {
+        config.optimization.minimizer.splice(
+            terserIndex,
+            1,
+            new ESBuildMinifyPlugin(options),
+        );
+    }
+}
+
+function useEsbuildLoader(config, options) {
+    const jsLoader = config.module.rules.find(rule => rule.test && rule.test.test('.js'));
+
+    if (jsLoader) {
+        if (!jsLoader.use) jsLoader.use = {};
+        jsLoader.use.loader = 'esbuild-loader';
+        jsLoader.use.options = options;
+    }
+}
 
 module.exports = withPWA({
     poweredByHeader: false,
@@ -32,11 +55,24 @@ module.exports = withPWA({
             }
         ];
     },
-    webpack: (config, { isServer }) => {
-        if (!isServer) config.resolve.fallback = {
-            fs: false,
-            path: false
-        };
+    webpack: (config, { isServer, webpack }) => {
+        if (!isServer) config.resolve.fallback = { fs: false, path: false };
+
+        config.mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+        config.plugins.push(
+            new webpack.ProvidePlugin({
+                React: 'react',
+            }),
+        );
+
+        useEsbuildMinify(config);
+
+        useEsbuildLoader(config, {
+            // Specify `tsx` if you're using TypeSCript
+            loader: 'jsx',
+            target: 'es2017',
+        });
 
         return config;
     }
