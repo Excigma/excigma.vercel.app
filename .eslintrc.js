6a9bde3ceeab2f7056dcb91d75da89e3b72f3841
Excigma <30280397+Excigma@users.noreply.github.com>
2021-06-02 22:57:27 +1200

    fix: fix eslint with nextjs plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,38 +9,17 @@ module.exports = {
         'plugin:react/recommended'
     ],
     'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
+        'ecmaFeatures': { 'jsx': true },
         'ecmaVersion': 12,
         'sourceType': 'module'
     },
-    'plugins': [
-        'react',
-        'mui-unused-classes'
-    ],
+    'plugins': ['react', 'mui-unused-classes', '@next/eslint-plugin-next'],
     'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'react/no-unescaped-entities': [
-            0
-        ],
-        'react/prop-types': [
-            0
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
+        'indent': ['error', 4],
+        'react/no-unescaped-entities': [0],
+        'react/prop-types': [0],
+        'linebreak-style': ['error', 'windows'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always']
     }
 };
