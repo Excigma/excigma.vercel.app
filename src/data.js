393d58699cd1567cff79e4c12a4dcaa059cc9603
Excigma <30280397+Excigma@users.noreply.github.com>
2021-06-14 14:14:45 +1200

    update: notes

--- src/data.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // read topics from dir automatically lmaOo :frown:
-    subjects: {
-        2021: ['9609_-_Business_Studies','9608_-_Computer_Science', '9708_-_Economics', '9702_-_Physics']
-    }
-};
