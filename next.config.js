dd7469957d8d25d405bad105928a7cd408fcb469
Excigma <30280397+Excigma@users.noreply.github.com>
2021-08-01 12:24:48 +1200

    fix: all pls

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ module.exports = withBundleAnalyzer(withMDX({
     },
     headers: () => {
         return [{
-            source: '*',
+            source: '/:anything*',
             headers: [{
                 key: 'Cross-Origin-Opener-Policy',
                 value: 'same-origin'
